fe6a78dbba26cfd7131e4683ddbd2222c4f0ecb5
Dustin Blackman <dev@dustinblackman.com>
2020-01-27 08:56:12 -0500

    add exit code option

--- pkg.go
+++ pkg.go
@@ -1,7 +1,21 @@
 package helloworld
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func SayHi() {
+	if len(os.Args) > 1 {
+		exitCode, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		os.Exit(exitCode)
+	}
+
 	fmt.Println("Hello World")
 }
